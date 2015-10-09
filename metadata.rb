062267f23182d2cc56f3d540bdc639e0e9f3d462
Zakeeruddin <zakeeruddin_mohammed@gap.com>
2015-10-09 11:40:10 -0700

    first commit

---
+++ metadata.rb
@@ -0,0 +1,7 @@
+name             'poisesample'
+maintainer       'YOUR_NAME'
+maintainer_email 'YOUR_EMAIL'
+license          'All rights reserved'
+description      'Installs/Configures poisesample'
+long_description 'Installs/Configures poisesample'
+version          '0.1.0'
